769f10335f9b4169a40dfa23dc1e71b72ca05622
m1webste <m1webste@ucsd.edu>
2017-10-13 13:54:04 -0700

    Added another db. Started python package.

---
+++ URA/upstream_regulator_analysis_project/ura/create_graph.py
@@ -0,0 +1,15 @@
+"""
+-------------------------------------------
+Author: Mikayla Webster (m1webste@ucsd.edu)
+Date: 10/13/17
+-------------------------------------------
+"""
+
+import numpy as np
+import pandas as pd
+import networkx as nx
+import scipy
+from scipy import stats
+import mygene
+import math
+
